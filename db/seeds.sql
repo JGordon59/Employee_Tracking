81dc45ed7a92a3bd926fb1c332c36762fab2ac20
Jonathan Gordon <jonathan.gordon59@gmail.com>
2022-09-08 14:50:50 -0400

    updated seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,21 +5,21 @@ VALUES ("Sales"),
 ("Legal");
 
 INSERT INTO roles (role_title, role_salary, department_id)
-VALUES ("Sales Lead", 100000, 001),
-("Salesperson", 80000, 001),
-("Lead Engineer", 150000, 002),
+VALUES ("Sales Lead", 35000, 001),
+("Sales Representative", 20000, 001),
+("Senior Software Engineer", 200000, 002),
 ("Software Engineer", 120000, 002),
-("Account Manager", 160000, 003),
-("Accountant", 125000, 003),
-("Legal Team Lead", 250000, 004),
-("Lawyer", 190000, 004);
+("Head of Accounting", 175000, 003),
+("Accountant", 120000, 003),
+("Chief Legal Officer", 275000, 004),
+("Lawyer", 200000, 004);
 
 INSERT INTO employees (employee_first_name, employee_last_name, role_id, manager_id)
-VALUES ("John", "Doe", 001, NULL),
-("Mike", "Chan", 002, 001),
-("Ashley", "Rodriguez", 003, NULL),
-("Kevin", "Tupik", 004, 003),
-("Kunal", "Singh", 005, NULL),
-("Malia", "Brown", 006, 005),
-("Sarah", "Lourd", 007, NULL),
-("Tom", "Allen", 008, 007);
+VALUES ("Jonathan", "Gordon", 001, NULL),
+("Chris", "Tucker", 002, 001),
+("Michelle", "Pineda", 003, NULL),
+("Harry", "Bhasin", 004, 003),
+("John", "Rabuse", 005, NULL),
+("Rigel", "Lopez", 006, 005),
+("Timothy", "Cabrera", 007, NULL),
+("Mackenzie", "Sanders", 008, 007);
